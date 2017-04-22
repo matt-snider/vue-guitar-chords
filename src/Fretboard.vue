83b1bb6c07642b509cb9dae3b0b34154e7efb802
Matt Snider <matt.snider@alum.utoronto.ca>
2017-04-22 12:13:19 +0200

    Extract fretboard to its own component

---
+++ src/Fretboard.vue
@@ -0,0 +1,29 @@
+<template>
+    <svg viewBox="0 0 100 100">
+        <!-- Fretboard edges -->
+        <line x1="0" y1="0" x2="100" y2="0"
+                stroke="black" stroke-width="1"/>
+        <line x1="0" y1="100" x2="100" y2="100"
+                stroke="black" stroke-width="1"/>
+        <line x1="100" y1="0" x2="100" y2="100"
+                stroke="black" stroke-width="0.5"/>
+
+        <!-- Nut -->
+        <line x1="0" y1="0" x2="0" y2="100"
+                stroke="black" stroke-width="2"/>
+
+        <!-- Frets -->
+        <line x1="20" y1="0" x2="20" y2="100" stroke="black" stroke-width="0.5"/>
+        <line x1="40" y1="0" x2="40" y2="100" stroke="black" stroke-width="0.5"/>
+        <line x1="60" y1="0" x2="60" y2="100" stroke="black" stroke-width="0.5"/>
+        <line x1="80" y1="0" x2="80" y2="100" stroke="black" stroke-width="0.5"/>
+
+        <!-- Strings -->
+        <line stroke="black" stroke-width="0.5" x1="0" y1="7.5" x2="100" y2="7.5"/>
+        <line stroke="black" stroke-width="0.5" x1="0" y1="24.5" x2="100" y2="24.5"/>
+        <line stroke="black" stroke-width="0.5" x1="0" y1="41.5" x2="100" y2="41.5"/>
+        <line stroke="black" stroke-width="0.5" x1="0" y1="58.5" x2="100" y2="58.5"/>
+        <line stroke="black" stroke-width="0.5" x1="0" y1="75.5" x2="100" y2="75.5"/>
+        <line stroke="black" stroke-width="0.5" x1="0" y1="92.5" x2="100" y2="92.5"/>
+    </svg>
+</template>
--- src/GuitarChord.vue
+++ src/GuitarChord.vue
@@ -13,42 +13,12 @@
             {{ note }}
         </text>
 
+        <!-- Nut label -->
         <text v-if="chord.nut !== 0" x="8" y="108" fill="black" font-size="8" text-anchor="start">
             {{ chord.nut }}
         </text>
 
-        <svg x="10" y="10" width="90" height="80" viewBox="0 0 100 100">
-            <!-- Fretboard edges -->
-            <line x1="0" y1="0" x2="100" y2="0"
-                    stroke="black" stroke-width="1"/>
-            <line x1="0" y1="100" x2="100" y2="100"
-                    stroke="black" stroke-width="1"/>
-            <line x1="100" y1="0" x2="100" y2="100"
-                    stroke="black" stroke-width="0.5"/>
-
-            <!-- Nut -->
-            <line x1="0" y1="0" x2="0" y2="100"
-                    stroke="black" stroke-width="2"/>
-
-            <!-- Frets -->
-            <line x1="20" y1="0" x2="20" y2="100" stroke="black" stroke-width="0.5"/>
-            <line x1="40" y1="0" x2="40" y2="100" stroke="black" stroke-width="0.5"/>
-            <line x1="60" y1="0" x2="60" y2="100" stroke="black" stroke-width="0.5"/>
-            <line x1="80" y1="0" x2="80" y2="100" stroke="black" stroke-width="0.5"/>
-
-            <!-- Strings: nested SVGs to make positioning easier -->
-            <line stroke="black" stroke-width="0.5" x1="0" y1="7.5" x2="100" y2="7.5"/>
-            <line stroke="black" stroke-width="0.5" x1="0" y1="24.5" x2="100" y2="24.5"/>
-            <line stroke="black" stroke-width="0.5" x1="0" y1="41.5" x2="100" y2="41.5"/>
-            <line stroke="black" stroke-width="0.5" x1="0" y1="58.5" x2="100" y2="58.5"/>
-            <line stroke="black" stroke-width="0.5" x1="0" y1="75.5" x2="100" y2="75.5"/>
-            <line stroke="black" stroke-width="0.5" x1="0" y1="92.5" x2="100" y2="92.5"/>
-
-            <circle cx="50" cy="42" r="4" stroke="black" stroke-width="3" fill="black" />
-            <text x="50" y="44" fill="white" font-size="8" text-anchor="middle">
-                1
-            </text>
-        </svg>
+        <fretboard x="10" y="10" width="90" height="80"></fretboard>
     </svg>
 </template>
 
@@ -62,7 +32,12 @@ const CHORD = {
     ],
 };
 
+import Fretboard from './Fretboard.vue';
+
 export default {
+    components: {
+        Fretboard,
+    },
     data() {
         return {
             chord: CHORD,
