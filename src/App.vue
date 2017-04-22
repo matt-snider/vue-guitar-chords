f68a44c6f98d71a2b1dd7c45d580dfb2549df4d6
Matt Snider <matt.snider@alum.utoronto.ca>
2017-04-22 14:10:11 +0200

    Update example chord

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
     <div id="app">
         <h1> Vue Guitar Chords </h1>
         <div style="width:250px; height:250px;">
-            <guitar-chord></guitar-chord>
+            <guitar-chord name="G chord" nut-position="0"
+                    tuning="EADGBE" :fretted="gChordNotes">
+            </guitar-chord>
         </div>
     </div>
 </template>
@@ -13,6 +15,16 @@ import GuitarChord from './GuitarChord.vue';
 export default {
     components: {
         GuitarChord,
+    },
+    data() {
+        return {
+            gChordNotes: [
+                { string: 0, fret: 3, finger: 2 },
+                { string: 1, fret: 2, finger: 1 },
+                { string: 4, fret: 3, finger: 3 },
+                { string: 5, fret: 3, finger: 4 },
+            ],
+        };
     },
 };
 </script>
