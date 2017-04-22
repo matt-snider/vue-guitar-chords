a8b95833c55d1d3c775e0d8c745efec12f0eaa50
Matt Snider <matt.snider@alum.utoronto.ca>
2017-04-22 11:46:38 +0200

    Clean up svg a bit and add fret lines

--- src/GuitarChord.vue
+++ src/GuitarChord.vue
@@ -1,29 +1,16 @@
 <template>
     <svg width="100%" viewBox="0 0 110 110">
-        <!-- Borders - left/right -->
+        <!-- Fretboard edges -->
         <line x1="10" y1="10" x2="100" y2="10"
                 stroke="black" stroke-width="1"/>
         <line x1="10" y1="99" x2="100" y2="99"
                 stroke="black" stroke-width="1"/>
-
-        <!-- Borders - bottom/top -->
-        <line x1="10" y1="10" x2="10" y2="100"
-                stroke="black" stroke-width="3"/>
         <line x1="100" y1="10" x2="100" y2="100"
-                stroke="black" stroke-width="0.5"/>
+                stroke="black" stroke-width="1"/>
 
-        <!-- Strings -->
-        <line v-for="s in strings" stroke="black" stroke-width="0.5"
-            :x1="s.x1" :y1="s.y1" :x2="s.x2" :y2="s.y2"/>
-
-        <!-- Fingers -->
-        <circle cx="50" cy="48" r="4" stroke="black" stroke-width="3" fill="black" />
-        <text x="50" y="50" fill="white" font-size="8" text-anchor="middle">
-            1
-        </text>
-
-
-        <!-- Nut position -->
+        <!-- Nut -->
+        <line x1="10" y1="10" x2="10" y2="100"
+                stroke="black" stroke-width="2"/>
         <text v-if="chord.nut !== 0" x="8" y="108" fill="black" font-size="8" text-anchor="start">
             {{ chord.nut }}
         </text>
@@ -40,7 +27,26 @@
         <text x="50" y="8" fill="black" font-size="8" text-anchor="middle">
             {{ chord.name }}
         </text>
-        Sorry, your browser does not support inline SVG.
+
+        <!-- Frets -->
+        <line x1="28" y1="10" x2="28" y2="100" stroke="black" stroke-width="0.5"/>
+        <line x1="46" y1="10" x2="46" y2="100" stroke="black" stroke-width="0.5"/>
+        <line x1="64" y1="10" x2="64" y2="100" stroke="black" stroke-width="0.5"/>
+        <line x1="82" y1="10" x2="82" y2="100" stroke="black" stroke-width="0.5"/>
+
+        <!-- Strings: nested SVGs to make positioning easier -->
+        <line stroke="black" stroke-width="0.5" x1="10" y1="15" x2="100" y2="15"/>
+        <line stroke="black" stroke-width="0.5" x1="10" y1="31" x2="100" y2="31"/>
+        <line stroke="black" stroke-width="0.5" x1="10" y1="47" x2="100" y2="47"/>
+        <line stroke="black" stroke-width="0.5" x1="10" y1="63" x2="100" y2="63"/>
+        <line stroke="black" stroke-width="0.5" x1="10" y1="79" x2="100" y2="79"/>
+        <line stroke="black" stroke-width="0.5" x1="10" y1="95" x2="100" y2="95"/>
+
+        <circle cx="55" cy="48" r="4" stroke="black" stroke-width="3" fill="black" />
+        <text x="55" y="50" fill="white" font-size="8" text-anchor="middle">
+            1
+        </text>
+
     </svg>
 </template>
 
@@ -54,24 +60,10 @@ const CHORD = {
     ],
 };
 
-// 16 between strings
-// 5 between string and fretboard edge
-const STRINGS = [
-    {x1: 10, y1: 15, x2: 100, y2: 15},
-
-    {x1: 10, y1: 31, x2: 100, y2: 31},
-    {x1: 10, y1: 47, x2: 100, y2: 47},
-    {x1: 10, y1: 63, x2: 100, y2: 63},
-    {x1: 10, y1: 79, x2: 100, y2: 79},
-
-    {x1: 10, y1: 95, x2: 100, y2: 95},
-];
-
 export default {
     data() {
         return {
             chord: CHORD,
-            strings: STRINGS,
         };
     },
 };
